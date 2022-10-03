254b80b48d96958ba087383a2178db2d47ea9097
jmackSosa <jmackey@potomacschool.org>
2022-10-03 08:58:14 -0400

    john is short

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 salary = int(input("Enter salary:"))
 
-
 #check is salary is valid input
 if salary <= 0:
     print("error, salary has to be positive")
 hours = int(input("Enter hours: "))
 
-#check if inputed hours are valid
+#check if inputed hours arwegronq3g[o[o2n42[132[o23[i135e valid
 if hours < 0:
-    print("error, hours has to be positive")
+    print("john is short")
 
 
 how_much = "your earned {} dollars "
@@ -21,7 +20,6 @@ if salary > 0 and hours >= 0:
     #check if hours were above 40
     if hours > 40:
         earned_over40 = (40 * salary) + ( (hours - 40) * salary * 1.5)
-        print(how_much.format(earned_over40))
 
 
 
